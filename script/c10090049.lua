adf692b2a74bacfe4983a8d316dd58790483b8f8
bcnsuri <bcnsuri@naver.com>
2026-05-15 19:40:22 +0900

    fasd

--- script/c10090049.lua
+++ script/c10090049.lua
@@ -5,7 +5,7 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_ADD_SETCODE)
-	e1:SetValue(0xd031)
+	e1:SetValue(0xd03)
 	c:RegisterEffect(e1)
 end
 function s.AndCookiecost(e,tp,eg,ep,ev,re,r,rp)
